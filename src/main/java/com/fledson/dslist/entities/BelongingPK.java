4190116b3fe8af092fcbe864e9a9a26299253063
Fledson Henrique <fledsonhenrique@gmail.com>
2023-11-14 22:55:19 -0300

    git commit -m ":sparkles: feat: Implementando modelo de dominio"

---
+++ src/main/java/com/fledson/dslist/entities/BelongingPK.java
@@ -0,0 +1,57 @@
+package com.fledson.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable /* precisa informar que essa classe auxiliar está encapsulando 2 chaves em 1 */
+public class BelongingPK {
+
+    /* mapeamento de campo para chave estrangeira na tabela*/
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    /* mapeamento de campo para chave estrangeira na tabela*/
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList list;
+
+    public BelongingPK() {
+    }
+
+    public BelongingPK(Game game, GameList list) {
+        this.game = game;
+        this.list = list;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getList() {
+        return list;
+    }
+
+    public void setList(GameList list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BelongingPK that)) return false;
+        return Objects.equals(game, that.game) && Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, list);
+    }
+}
