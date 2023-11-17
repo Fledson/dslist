fb1a53aa20daa5f23b6cb433ad601a489c8ea150
Fledson Henrique <fledsonhenrique@gmail.com>
2023-11-16 21:05:52 -0300

    :sparkles: feat: adicionando configuração de cors

---
+++ src/main/java/com/fledson/dslist/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.fledson.dslist.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+public class WebConfig {
+
+    /* pegando o valor da variavel de ambiente e injetando na variavel corsOrigins */
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    /* metodo que faz a configuração do cors */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
+
+}
